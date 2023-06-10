3c7bb11171b4911afbdc092a7c6fec84553c892d
VincentP-EFREI <vincent.pierro@efrei.net>
2023-06-10 13:48:13 +0200

    Finish shape set menu

--- src/fr/vincent/CLI.java
+++ src/fr/vincent/CLI.java
@@ -1,8 +1,7 @@
 package fr.vincent;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
 
 public final class CLI {
 
@@ -12,7 +11,7 @@ public final class CLI {
         String val = "\n";
         boolean error;
         do {
-
+            cls();
             System.out.print("Enter a character: ");
             val = Main.scanner.next();
 
@@ -25,89 +24,159 @@ public final class CLI {
         return val.charAt(0);
     }
 
-    public static Point setPoint(){
-        int x = 0, y = 0;
+    private static int valAffectation(String toPrintString, String toAskString){
+        int val = 0;
         boolean error;
         do {
             error = false;
             try {
-                System.out.println("You need to give x and y.");
-                System.out.print("x : ");
-                x = Main.scanner.nextInt();
+                cls();
+                System.out.print(toPrintString);
+                System.out.print(toAskString);
+                val = Main.scanner.nextInt();
             } catch (InputMismatchException e) {
                 Main.scanner.nextLine();
                 error = true;
             }
         }while (error);
-        do {
-            error = false;
-            try {
-                System.out.println("You need to give x and y.");
-                System.out.printf("x : %d\n", x);
-                System.out.print("y : ");
-                y = Main.scanner.nextInt();
-            } catch (InputMismatchException e) {
-                Main.scanner.nextLine();
-                error = true;
-            }
-        }while (error);
+        return val;
+    }
+
+    public static Point setPoint()
+    {
+        int x = 0, y = 0;
+
+        String toPrintString = "You need to give x and y.\n";
+        String toAskString = "x : ";
+        x = valAffectation(toPrintString, toAskString);
+
+        toPrintString = toPrintString + "x : " + x + "\n";
+        toAskString = "y : ";
+        y = valAffectation(toPrintString, toAskString);
+
         return new Point(x,y);
     }
 
-    public static Line setLine(){
+    public static Line setLine()
+    {
         int x1 = 0, x2 = 0, y1 = 0, y2 =0;
-        boolean error;
-        do {
-            error = false;
-            try {
-                System.out.println("You need to give x1, y1, x2 and y2.");
-                System.out.print("x1 : ");
-                x1 = Main.scanner.nextInt();
-            } catch (InputMismatchException e) {
-                Main.scanner.nextLine();
-                error = true;
-            }
-        }while (error);
-        do {
-            error = false;
-            try {
-                System.out.println("You need to give x1, y1, x2 and y2.");
-                System.out.printf("x1 : %d\n", x1);
-                System.out.print("y1 : ");
-                y1 = Main.scanner.nextInt();
-            } catch (InputMismatchException e) {
-                Main.scanner.nextLine();
-                error = true;
-            }
-        }while (error);
-        do {
-            error = false;
-            try {
-                System.out.println("You need to give x1, y1, x2 and y2.");
-                System.out.printf("x1 : %d\n", x1);
-                System.out.printf("y1 : %d\n", y1);
-                System.out.print("x2 : ");
-                x2 = Main.scanner.nextInt();
-            } catch (InputMismatchException e) {
-                Main.scanner.nextLine();
-                error = true;
-            }
-        }while (error);
-        do {
-            error = false;
-            try {
-                System.out.println("You need to give x1, y1, x2 and y2.");
-                System.out.printf("x1 : %d\n", x1);
-                System.out.printf("y1 : %d\n", y1);
-                System.out.printf("x2 : %d\n", x2);
-                System.out.print("y2 : ");
-                y2 = Main.scanner.nextInt();
-            } catch (InputMismatchException e) {
-                Main.scanner.nextLine();
-                error = true;
-            }
-        }while (error);
+
+        String toPrintString = "You need to give x1, y1, x2 and y2.\n";
+        String toAskString = "x1 : ";
+        x1 = valAffectation(toPrintString, toAskString);
+
+        toPrintString = toPrintString + "x1 : " + x1 + "\n";
+        toAskString = "y1 : ";
+        y1 = valAffectation(toPrintString, toAskString);
+
+        toPrintString = toPrintString + "y1 : " + y1 + "\n";
+        toAskString = "x2 : ";
+        x2 = valAffectation(toPrintString, toAskString);
+
+        toPrintString = toPrintString + "x2 : " + x2 + "\n";
+        toAskString = "y2 : ";
+        y2 = valAffectation(toPrintString, toAskString);
+
         return new Line(new Point(x1, y1), new Point(x2, y2));
     }
+
+    public static Square setSquare()
+    {
+        int x = 0, y = 0, length = 0;
+
+        String toPrintString = "You need to give x, y and length.\n";
+        String toAskString = "x : ";
+        x = valAffectation(toPrintString, toAskString);
+
+        toPrintString = toPrintString + "x : " + x + "\n";
+        toAskString = "y : ";
+        y = valAffectation(toPrintString, toAskString);
+
+        toPrintString = toPrintString + "y : " + y + "\n";
+        toAskString = "length : ";
+        length = valAffectation(toPrintString, toAskString);
+
+        return new Square(new Point(x, y), length);
+    }
+
+    public static Rectangle setRectangle()
+    {
+        int x = 0, y = 0, length = 0, width = 0;
+
+        String toPrintString = "You need to give x, y, length and width.\n";
+        String toAskString = "x : ";
+        x = valAffectation(toPrintString, toAskString);
+
+        toPrintString = toPrintString + "x : " + x + "\n";
+        toAskString = "y : ";
+        y = valAffectation(toPrintString, toAskString);
+
+        toPrintString = toPrintString + "y : " + y + "\n";
+        toAskString = "length : ";
+        length = valAffectation(toPrintString, toAskString);
+
+        toPrintString = toPrintString + "length : " + length + "\n";
+        toAskString = "width : ";
+        width = valAffectation(toPrintString, toAskString);
+
+        return new Rectangle(new Point(x, y), length, width);
+    }
+
+    public static Circle setCircle()
+    {
+        int x = 0, y = 0, radius = 0;
+
+        String toPrintString = "You need to give x, y, length and width.\n";
+        String toAskString = "x : ";
+        x = valAffectation(toPrintString, toAskString);
+
+        toPrintString = toPrintString + "x : " + x + "\n";
+        toAskString = "y : ";
+        y = valAffectation(toPrintString, toAskString);
+
+        toPrintString = toPrintString + "y : " + y + "\n";
+        toAskString = "radius : ";
+        radius = valAffectation(toPrintString, toAskString);
+
+        return new Circle(new Point(x, y), radius);
+    }
+
+    public static Polygon setPolygon()
+    {
+        ArrayList<Point> pointList = new ArrayList<>();
+        int x = 0, y = 0, n = 0;
+
+        String toPrintString = "You need to give the number of point.\n";
+        String toAskString = "number of point : ";
+        n = valAffectation(toPrintString, toAskString);
+
+        toPrintString = "";
+        for(int i = 0; i < 2 * n; i += 2){
+
+            toPrintString = toPrintString + "You need to give the x"+(i / 2 + 1)+" and y"+(i / 2 + 1)+".\n";
+            toAskString = "x"+(i / 2 + 1)+" : ";
+            x = valAffectation(toPrintString, toAskString);
+
+            toPrintString = toPrintString + "x"+ (i / 2 + 1) + " : " + x + "\n";
+            toAskString = "y"+(i / 2 + 1)+" : ";
+            y = valAffectation(toPrintString, toAskString);
+
+            toPrintString = toPrintString + "y"+ (i / 2 + 1) + " : " + y + "\n";
+            pointList.add(new Point(x, y));
+        }
+
+        return new Polygon(pointList);
+    }
+
+    public static void cls()
+    {
+        try {
+            new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor();
+        }
+        catch(Exception E) {
+            System.out.println(E);
+        }
+    }
+
 
 }
--- src/fr/vincent/Main.java
+++ src/fr/vincent/Main.java
@@ -1,9 +1,5 @@
 package fr.vincent;
 
-import fr.vincent.CLI;
-
-import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -24,22 +20,18 @@ public class Main {
 
             switch (Character.toUpperCase(choice)) {
                 case 'A' -> {
-
                     choice = CLI.boundedChoice("Your choice : ", 'A', 'F', false);
                     switch (Character.toUpperCase(choice)) {
                         case 'A' -> screen_area.add_shape_to_area(CLI.setPoint());
                         case 'B' -> screen_area.add_shape_to_area(CLI.setLine());
-                        case 'C' -> {
-                        }
-                        case 'D' -> {
-                        }
-                        case 'E' -> {
-                        }
-                        case 'F' -> {
-                        }
+                        case 'C' -> screen_area.add_shape_to_area(CLI.setSquare());
+                        case 'D' -> screen_area.add_shape_to_area(CLI.setRectangle());
+                        case 'E' -> screen_area.add_shape_to_area(CLI.setCircle());
+                        case 'F' -> screen_area.add_shape_to_area(CLI.setPolygon());
                     }
                 }
                 case 'B' -> {
+
                 }
                 case 'C' -> {
                 }
@@ -53,62 +45,6 @@ public class Main {
                 }
             }
         }
-
         scanner.close();
-
-        /*
-        Area arre = new Area(45,45);
-
-        Point point = new Point(2,2);
-        point.print_point();
-        System.out.println(point.shape_type);
-        arre.add_shape_to_area(point);
-
-        Point p1 = new Point(4,2);
-        Point p2 = new Point(25,9);
-        Line line = new Line(p1, p2);
-        line.print();
-        System.out.println(line.shape_type);
-        arre.add_shape_to_area(line);
-
-
-        Point p_square = new Point(2,9);
-        Square square = new Square(p_square, 5);
-        square.print();
-        System.out.println(square.shape_type);
-        arre.add_shape_to_area(square);
-
-
-        Point p_rectangle = new Point(2,19);
-        Rectangle rectangle = new Rectangle(p_rectangle, 3, 6);
-        rectangle.print();
-        System.out.println(rectangle.shape_type);
-        arre.add_shape_to_area(rectangle);
-
-
-        Point p_circle = new Point(20,20);
-        Circle circle = new Circle(p_circle, 10);
-        circle.print();
-        System.out.println(circle.shape_type);
-        arre.add_shape_to_area(circle);
-
-
-        ArrayList<Point> p_polygon = new ArrayList<>();
-        p_polygon.add(new Point(1, 43));
-        p_polygon.add(new Point(4, 43));
-        p_polygon.add(new Point(10, 41));
-        Polygon polygon = new Polygon(p_polygon);
-        polygon.print();
-        System.out.println(polygon.shape_type);
-        arre.add_shape_to_area(polygon);
-
-        for (int i = 0; i< arre.nb_shape; i++){
-            System.out.println(arre.shapes.get(i).pixel_arr.size());
-        }
-
-        arre.draw_area();
-        arre.print_area();
-        */
-
     }
 }
